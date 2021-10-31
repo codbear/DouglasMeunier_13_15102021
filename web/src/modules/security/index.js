8ee680e8d70923414bee1c00a1a98aeaff51efc8
codbear <douglas.meunier@openclassrooms.com>
2021-10-31 17:56:33 +0100

    feat(redux): create and connect store

--- web/src/modules/security/index.js
+++ web/src/modules/security/index.js
@@ -1,1 +1,7 @@
 export { default as LoginScreen } from './screens/LoginScreen';
+
+export { default as securityReducer } from './reducers';
+export { default as securityMiddleware } from './middleware';
+export { default as securityActions } from './actions';
+
+export { default as useIsConnected } from './hooks/useIsConnected';

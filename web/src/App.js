5acca94d099250b62a8b78cbf9eb058f9b1d9592
codbear <douglas.meunier@openclassrooms.com>
2021-10-31 22:50:25 +0100

    fix(App): fetch user info on app loading

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,24 @@
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { Router } from './router';
-import store from './lib/store';
+import { useCurrentUser, userActions } from './modules/security';
 
 library.add(faUserCircle, faSignOutAlt);
 
-const App = () => (
-  <Provider store={store}>
-    <Router />
-  </Provider>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    if (!currentUser) {
+      dispatch(userActions.fetchUserRequest());
+    }
+  }, [currentUser, dispatch]);
+
+  return <Router />;
+};
 
 export default App;
